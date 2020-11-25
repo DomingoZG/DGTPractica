d4b8ed0f0289df24ab408663124870df16676ff5
DomingoZG <57489303+DomingoZG@users.noreply.github.com>
2020-11-25 20:06:47 +0100

    form
    Tengo que ver lo de ocultarlos y mostrarlos.

---
+++ codigo.js
@@ -0,0 +1,23 @@
+
+
+
+/* MOSTRAR LOS FORMULARIOS */
+/*
+function ocultar() 
+{
+    let divConjunto = document
+      .querySelector("#divTotal")
+      .querySelectorAll(".divHijo");
+      divConjunto.forEach(value =>
+         {
+      let x = value;
+      x.style.display = "none";
+    });
+  }
+  
+  function mostrar(x) 
+  {
+    ocultar();
+    document.querySelector(x).style.display = "block";
+  }
+  */
