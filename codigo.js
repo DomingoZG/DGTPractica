c46961eb2a610f2ac8e83ea6dc8414f4f090d5c6
AlvaroJimenez13 <66470657+AlvaroJimenez13@users.noreply.github.com>
2020-11-28 13:03:24 +0100

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -7,40 +7,20 @@ function altaConductor() {
   formConductor.style.display = "block";
   formGuardia.style.display = "none";
   formMulta.style.display = "none";
-    // divListadoOrdenado.style.display = "none";
+    
 
 }
 function altaGuardiaCivil() {
   formConductor.style.display = "none";
   formGuardia.style.display = "block";
   formMulta.style.display = "none";
-    // divListadoOrdenado.style.display = "none";
+    
 
 }
 function altaMulta() {
   formConductor.style.display = "none";
   formGuardia.style.display = "none";
   formMulta.style.display = "block";
-    // divListadoOrdenado.style.display = "none";
+    
 
 }
-/*
-function ocultar() 
-{
-    let divConjunto = document
-      .querySelector("#divTotal")
-      .querySelectorAll(".divHijo");
-      divConjunto.forEach(value =>
-         {
-      let x = value;
-      x.style.display = "none";
-    });
-  }
-  
-  function mostrar(x) 
-  {
-    ocultar();
-    document.querySelector(x).style.display = "block";
-  }
-  */
-
